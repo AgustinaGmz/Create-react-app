2fdd7eb5fd8b879044176f47a148d05f6272b708
AgustinaGimenez <agustina.gmenz@gmail.com>
2022-04-18 08:52:30 -0300

    Api giphy y react

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from './Navigation';
+import Gif from './Gif';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Gif />
+    </div>
+  );
+}
+
+export default App;
+
+/* mi api key de gif */
+/* IxFu8U7EG73YOZdDot9bA2MUnGPvyLrk */
